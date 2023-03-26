2bc0e2e3af444936a6d424de09c53e469cad44b2
elijah-droid <mukisaelijah293@gmail.com>
2023-03-26 17:10:58 +0300

    a few tweaks

---
+++ Edumetrics/static/js/next.js
@@ -0,0 +1,35 @@
+(function ($) {
+    "use strict";
+
+
+    // Facts counter
+    $('[data-toggle="counter-up"]').counterUp({
+        delay: 10,
+        time: 2000
+    });
+
+
+    // Typed Initiate
+    if ($('.typed-text-output').length == 1) {
+        var typed_strings = $('.typed-text').text();
+        var typed = new Typed('.typed-text-output', {
+            strings: typed_strings.split(', '),
+            typeSpeed: 100,
+            backSpeed: 20,
+            smartBackspace: false,
+            loop: true
+        });
+    }
+
+
+    
+    // Skills
+    $('.skill').waypoint(function () {
+        $('.progress .progress-bar').each(function () {
+            $(this).css("width", $(this).attr("aria-valuenow") + '%');
+        });
+    }, {offset: '80%'});
+
+;
+})(jQuery);
+
