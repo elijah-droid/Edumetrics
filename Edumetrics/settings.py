c3f32f90707a86584ff0a2209384b9e014efb34e
elijah-droid <mukisaelijah293@gmail.com>
2023-03-27 10:04:18 +0300

    static files

--- Edumetrics/settings.py
+++ Edumetrics/settings.py
@@ -158,7 +158,7 @@ USE_TZ = False
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+STATIC_ROOT = os.path.join(BASE_DIR, 'Edumetrics/static')
 
 MEDIA_URL = ''
 MEDIA_ROOT = os.path.join(BASE_DIR, '')
