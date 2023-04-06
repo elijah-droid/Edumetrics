b4baf7ec0992e31a62120b5d1f3dadc1bbbf6007
elijah-droid <mukisaelijah293@gmail.com>
2023-04-06 18:17:13 +0300

    Change UI

--- Edumetrics/settings.py
+++ Edumetrics/settings.py
@@ -52,7 +52,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'django.contrib.sites',
     'Auth',
     'Examinations',
     'Parents',
