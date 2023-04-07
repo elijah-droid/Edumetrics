849c92e8bba7c08c31417779dc607250e7d70b73
elijah-droid <mukisaelijah293@gmail.com>
2023-04-07 10:19:13 +0300

    graphical presentations

--- Edumetrics/settings.py
+++ Edumetrics/settings.py
@@ -36,7 +36,7 @@ CELERY_BEAT_SCHEDULE = {
 SECRET_KEY = 'django-insecure-#4xjdr0@zw4nzy(697deby!li%wop2zkl%vf4hlt$9e$r^*z5('
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 ALLOWED_HOSTS = ['192.168.137.1', '127.0.0.1']
 
 RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
