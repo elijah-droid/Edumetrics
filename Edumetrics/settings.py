9502132a681af111717f162d1f97f5ae084c2053
elijah-droid <mukisaelijah293@gmail.com>
2023-05-04 04:09:51 +0300

    fixes

--- Edumetrics/settings.py
+++ Edumetrics/settings.py
@@ -65,7 +65,7 @@ SECRET_KEY = 'django-insecure-#4xjdr0@zw4nzy(697deby!li%wop2zkl%vf4hlt$9e$r^*z5(
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
-ALLOWED_HOSTS = ['192.168.137.1', '127.0.0.1', 'edu-metrics.com']
+ALLOWED_HOSTS = ['192.168.137.1', '127.0.0.1', 'edu-metrics.com', 'shop.edu-metrics.com']
 
 LOGIN_URL = '/'
 
