6314171d9b8e174382756042dbe03574c00f558b
Elijah <mukisaelijah293@gmail.com>
2023-05-10 00:27:47 -0700

    Admin Middleware

---
+++ Edumetrics/admin_middleware.py
@@ -0,0 +1,32 @@
+from django.core.mail import send_mail
+from django.contrib.auth import authenticate, login, logout
+from django.shortcuts import redirect
+from SchoolAdministrators.models import Adminship
+from django.contrib import messages
+
+
+
+
+class AdminMiddleware:
+    def __init__(self, get_response):
+        self.get_response = get_response
+
+    def __call__(self, request):
+        changed = False
+        try:
+            if request.user.schooladministrator:
+                adminship = AdminShip.objects.get(School=request.user.schooladministrator.current_school, Admin=request.user.schooladministrator)
+                if adminship.Groups.all() != user.groups.all():
+                    adminship.Goups.set(user.groups.all())
+                    changed = True
+                if adminship.super_admin != user.is_superuser:
+                    adminship.super_admin = user.is_superuser
+                    adminship.save()
+                    changed = True
+        except:
+            pass
+        if changed:
+            messages.success(request, 'You Pemissions were reviewed')
+        return self.get_response(request)
+        
+    
