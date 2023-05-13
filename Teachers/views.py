9ee7949f94a461b594991d755d94f1c9a4b63284
Elijah <mukisaelijah293@gmail.com>
2023-05-13 07:34:16 -0700

    Teacher Registration

--- Teachers/views.py
+++ Teachers/views.py
@@ -8,6 +8,7 @@ from django.contrib.auth.models import User
 from django.core.mail import send_mail
 from django.contrib import messages
 from django.core.paginator import Paginator
+from django.core.exceptions import ObjectDoesNotExist
 
 
 @login_required
