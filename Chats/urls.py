8c051c72ba83f4bf8f6c46b2665b8fd1d310ed56
Elijah <mukisaelijah293@gmail.com>
2023-07-24 15:40:48 +0300

    Debug

--- Chats/urls.py
+++ Chats/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
-from .views import send_message
+from .views import send_message, login
 
 urlpatterns = [
     path('send-message/', send_message, name='send-message'),
-    
+    path('login/', login, name='chats-login')
 ]
--- Chats/views.py
+++ Chats/views.py
@@ -1,8 +1,21 @@
 from django.shortcuts import render, redirect
 from django.http import JsonResponse
+from django.contrib.auth import authenticate, login, logout
 
 
-def send_message(request, ):
+def login(request):
+    if request.method == 'POST':
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+        user = authenticate(request, username=email, password=password)
+        if user is not None:
+            return JsonResponse({'user': str(user)})
+        else:
+            return JsonResponse({'user': None})
+
+
+
+def send_message(request):
     if request.method == 'POST':
         data = request.POST
         return JsonResponse(data)
