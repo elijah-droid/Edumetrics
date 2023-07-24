f4a8b36fe21c4816029961ed64533d085eadc243
Elijah <mukisaelijah293@gmail.com>
2023-07-24 16:01:19 +0300

    Debug

--- Edumetrics/settings.py
+++ Edumetrics/settings.py
@@ -138,7 +138,6 @@ MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
-    'corsheaders.middleware.CorsMiddleware',
      'django.middleware.common.CommonMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
